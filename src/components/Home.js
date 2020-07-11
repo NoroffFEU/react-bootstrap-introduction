4e37d0209f000635c08794cffde6c9e7ec669f4a
cnnrbrn <connorbobrien@gmail.com>
2020-07-11 20:20:25 +0200

    Add part 2 files

--- src/components/Home.js
+++ src/components/Home.js
@@ -2,104 +2,52 @@ import React from "react";
 import Navbar from "react-bootstrap/Navbar";
 import Form from "react-bootstrap/Form";
 import Jumbotron from "react-bootstrap/Jumbotron";
-import Button from "react-bootstrap/Button";
 import Container from "react-bootstrap/Container";
 import Row from "react-bootstrap/Row";
 import Col from "react-bootstrap/Col";
-import Tabs from "react-bootstrap/Tabs";
-import Tab from "react-bootstrap/Tab";
-import Accordion from "react-bootstrap/Accordion";
-import Card from "react-bootstrap/Card";
+import ContactForm from "./ContactForm";
+import ContentTabs from "./ContentTabs";
+import ContentAccordion from "./ContentAccordion";
 
 function Home() {
     return (
         <>
-            <Navbar bg="light" expand="lg">
-                <Navbar.Brand href="#home">React-Bootstrap</Navbar.Brand>
+            <Navbar bg="dark" expand="lg" variant="dark">
+                <Container>
+                    <Navbar.Brand href="#home">React-Bootstrap</Navbar.Brand>
 
-                <Form inline>
-                    <Form.Control type="text" placeholder="Search" className="mr-sm-2" />
-                </Form>
+                    <Form inline className="form-search">
+                        <Form.Control type="text" placeholder="Search" className="mr-sm-2" />
+                    </Form>
+                </Container>
             </Navbar>
 
-            <Jumbotron>
-                <h1>Hello, world!</h1>
+            <Jumbotron style={{ backgroundImage: "url(images/jumbo.png)" }}>
+                <Container>
+                    <h1>Hello, world!</h1>
+                </Container>
             </Jumbotron>
 
-            <h2>Stop Buying Things</h2>
-            <p>
-                Nam tincidunt, sapien nec congue porta, tellus risus ullamcorper mi, a rutrum justo eros pretium libero. Nullam vel enim id mauris eleifend finibus et ac orci. Aliquam metus massa,
-                aliquam quis arcu sit amet, consectetur faucibus urna. Suspendisse massa diam, efficitur eu massa euismod, pretium lacinia magna. Donec mi orci, sollicitudin in luctus a, varius eget
-                massa.
-            </p>
+            <Container>
+                <h2>Stop Buying Things</h2>
+                <p>
+                    Nam tincidunt, sapien nec congue porta, tellus risus ullamcorper mi, a rutrum justo eros pretium libero. Nullam vel enim id mauris eleifend finibus et ac orci. Aliquam metus massa,
+                    aliquam quis arcu sit amet, consectetur faucibus urna. Suspendisse massa diam, efficitur eu massa euismod, pretium lacinia magna. Donec mi orci, sollicitudin in luctus a, varius
+                    eget massa.
+                </p>
 
-            <h3>Tell us something new</h3>
+                <h3>Tell us something new</h3>
+            </Container>
 
             <Container>
                 <Row>
                     <Col md={6}>
-                        <Form>
-                            <Form.Group>
-                                <Form.Control type="text" placeholder="Name" />
-                            </Form.Group>
-
-                            <Form.Group>
-                                <Form.Control type="email" placeholder="Email" />
-                            </Form.Group>
-
-                            <Form.Control as="textarea" rows="6" placeholder="Message" />
-
-                            <Button variant="primary" type="submit">
-                                Submit
-                            </Button>
-                        </Form>
+                        <ContactForm />
                     </Col>
                     <Col md={6}>
-                        <Accordion defaultActiveKey="0" className="d-md-none">
-                            <Card>
-                                <Card.Header>
-                                    <Accordion.Toggle as={Button} variant="link" eventKey="0">
-                                        Lorem
-                                    </Accordion.Toggle>
-                                </Card.Header>
-                                <Accordion.Collapse eventKey="0">
-                                    <Card.Body>Hello! I'm the body</Card.Body>
-                                </Accordion.Collapse>
-                            </Card>
-                            <Card>
-                                <Card.Header>
-                                    <Accordion.Toggle as={Button} variant="link" eventKey="1">
-                                        Ipsem
-                                    </Accordion.Toggle>
-                                </Card.Header>
-                                <Accordion.Collapse eventKey="1">
-                                    <Card.Body>Hello! I'm another body</Card.Body>
-                                </Accordion.Collapse>
-                            </Card>
-                            <Card>
-                                <Card.Header>
-                                    <Accordion.Toggle as={Button} variant="link" eventKey="1">
-                                        Dolor
-                                    </Accordion.Toggle>
-                                </Card.Header>
-                                <Accordion.Collapse eventKey="1">
-                                    <Card.Body>Hello! I'm another body</Card.Body>
-                                </Accordion.Collapse>
-                            </Card>
-                        </Accordion>
-
+                        <ContentAccordion />
                         <div className="d-none d-md-block">
-                            <Tabs defaultActiveKey="one" id="uncontrolled-tab-example">
-                                <Tab eventKey="one" title="Lorem">
-                                    Tab 1
-                                </Tab>
-                                <Tab eventKey="two" title="Ipsem">
-                                    Tab 2
-                                </Tab>
-                                <Tab eventKey="three" title="Dolor">
-                                    Tab 3
-                                </Tab>
-                            </Tabs>
+                            <ContentTabs />
                         </div>
                     </Col>
                 </Row>
